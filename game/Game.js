b59d748fb4eb41fecec4b2f9bb877e954279d57a
competitor-01 <55604744+kantans@users.noreply.github.com>
2026-03-19 00:48:46 +0200

    feat/min-max-algorithm

--- game/Game.js
+++ game/Game.js
@@ -23,16 +23,48 @@ export class Game {
 	generateNumber() {
 		const [max, min] = range;
 		const numbers = [];
+	computerMove() {
+		if(this.algorithm === 'minmax') {
+			this.current = this.#getBestMove(this.current);
+			this.history.unshift(`Dators dala ar ${this.current.move}. Rezultāts: Cilvēks: ${this.current.humanScore}, Dators: ${this.current.computerScore}. (skaitlis: ${this.current.number})`);
+		}
+
+		if(this.algorithm === 'alfabeta') {
+
+		}
+	}
 
 		// šeit veicam ciklu, līdz tiek atrasti 5 randomizēti skaitļi kas dalās ar 2 un 3
 		while(numbers.length < 5) {
 			const generatedNumber = Math.floor(Math.random() * (max - min + 1)) + min
+	#getBestMove(node) {
+		let bestScore = -Infinity;
+		let bestChild = null;
 
 			if((generatedNumber % 3 === 0) && (generatedNumber % 2 === 0)) {
 				numbers.push(generatedNumber);
+		for (const child of node.children) {
+			const score = this.#minimax(child);
+			if (score > bestScore) {
+				bestScore = score;
+				bestChild = child;
 			}
 		}
 
 		return numbers;
+		return bestChild;
+	}
+
+	#minimax(node) {
+		if (node.children.length === 0) {
+			return node.computerScore - node.humanScore;
+		}
+
+		if (node.turn === 'computer') {
+			return Math.max(...node.children.map(c => this.#minimax(c)));
+		} else {
+			return Math.min(...node.children.map(c => this.#minimax(c)));
+		}
+	}
 	}
 }
