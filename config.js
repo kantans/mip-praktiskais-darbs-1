b1485265a7fb6b4f5c8d92ef80a6b119b73aba1e
Littleunidragon <a.voropajeva3@gmail.com>
2026-03-26 17:34:20 +0200

    added: partial tree generation; HNF to general func; best path

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-export const range = [10000, 20000];
+export const range = [10000, 100000];
+export const maxTreeDepth = 4; // konstante, kas atbild par spēles koka ģenerēšanas dziļumu
+// labāk nelikt 1 vai 2, jo datoram būs pārāk maz informācijas un tas nespēs izdarīt gājienu
--- game/GameTree.js
+++ game/GameTree.js
@@ -1,22 +1,25 @@
 import {GameNode} from "./GameNode.js";
+import { maxTreeDepth } from "../config.js";
 
 export class GameTree {
 	constructor(startingNumber, startingPlayer) {
-		this.root = this.#buildTree(startingNumber, 0, 0, startingPlayer)
+		this.maxDepth = maxTreeDepth; // pievienoju ģenerēšanas ierobežojumu
+		this.root = this.#buildTree(startingNumber, 0, 0, startingPlayer, 0);
 	}
 
-	#buildTree(number, humanScore, computerScore, turn) {
+	#buildTree(number, humanScore, computerScore, turn, depth) {
 		const node = new GameNode(number, humanScore, computerScore, turn);
 
-		if (number <= 10) return node; // beigu nosacījums
+		if (number <= 10 || depth >= this.maxDepth) return node; // beigu nosacījums
 
 		if (number % 2 === 0) {
 			const nextTurn = turn === 'human' ? 'computer' : 'human';
 			const h = turn === 'human' ? humanScore : humanScore + 2; // ja dala cilvēks, datoram +2
 			const c = turn === 'computer' ? computerScore : computerScore + 2; // ja dala dators, cilvēkam +2
 
-			const child = this.#buildTree(number / 2, h, c, nextTurn);
-			child.move = '/2'
+			const child = this.#buildTree(number / 2, h, c, nextTurn, depth+1);
+			child.parent = node; // saglabāju priekšteci, lai varētu pēc spēles beigām atrast uzvaru nesošo ceļu
+			child.move = '/2';
 			node.children.push(child);
 		}
 
@@ -25,8 +28,9 @@ export class GameTree {
 			const h = turn === 'human' ? humanScore + 3 : humanScore; // ja dala cilvēks, cilvēkam + 3
 			const c = turn === 'computer' ? computerScore + 3 : computerScore; // ja dala dators, datoram +3
 
-			const child = this.#buildTree(number / 3, h, c, nextTurn);
-			child.move = '/3'
+			const child = this.#buildTree(number / 3, h, c, nextTurn, depth+1);
+			child.parent = node;
+			child.move = '/3';
 			node.children.push(child);
 		}
 
